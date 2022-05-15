279ec03d89446437b9e8afd724b56da1bf56ca1c
Rahulranjan916 <rahulranjan91622@gmail.com>
2022-05-15 13:27:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Assignment = () => {
+    const [data, setData] = useState([])
+    var limits = 20
+    var offset = 1
 
-export default App;
+    const loadMoreData = (sl, offset) => {
+        axios.get(`https://api.seatgeek.com/2/events?client_id=MjQ1OTk2ODB8MTYzNzczNDkzNy45MDAwMjE&page=${offset}&per_page=${sl}`)
+            .then((res) => {
+                res.data.events.forEach((sl) => {
+                    setData(e => [...e, sl])
+                })
+            })
+    }
+
+    const handleScroll = (e) => {
+        if (window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight) {
+            offset = offset + 1;
+            loadMoreData(limits, offset)
+        }
+    }
+
+    useEffect(() => {
+        axios.get(`https://api.seatgeek.com/2/events?client_id=MjQ1OTk2ODB8MTYzNzczNDkzNy45MDAwMjE&page=1&per_page=${limits}`)
+            .then((res) => {
+                setData(res.data.events)
+            })
+        window.addEventListener('scroll', handleScroll)
+    }, [])
+
+    return (
+        <div>
+            {
+                data && data.map((doc) =>
+                    <div style={{ padding: '0 2%' }}>
+                        <div>
+                            <h6>{doc.type}</h6>
+                            <h6>{doc.id}</h6>
+                            <h6>{doc.title}</h6>
+                            <h6>{doc.popularity}</h6>
+                            <h6>{doc.url}</h6>
+                            <h6>{new Date(doc.datetime_local).toUTCString()}</h6>
+                        </div>
+                        <hr />
+                    </div>
+                )
+            }
+        </div>
+    );
+};
+
+export default Assignment;
